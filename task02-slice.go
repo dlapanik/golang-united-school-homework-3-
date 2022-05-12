9e17c8b78841f8c9b63d81de96e4f277e05e62bb
Dzianis Lapanik <dzianis_lapanik@epam.com>
2022-05-12 20:22:40 +0300

    homework. attempt #1

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,14 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
+
+	result = make([]int64, len(input))
+	j := 0
+
+	for i := len(result) - 1; i >= 0; i-- {
+		result[i] = input[j]
+		j++
+	}
+
 	return
 }
